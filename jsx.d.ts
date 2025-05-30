521dd5a3d76e7c1163d79ee7a909bd81804a478c
theplayzzz <play-felix@hotmail.com>
2025-05-30 18:27:03 +0000

    chore: update ESLint and TypeScript configurations - Disabled the no-page-custom-font rule in ESLint and added jsx.d.ts to the TypeScript include paths for improved type checking and compatibility with JSX files.

---
+++ jsx.d.ts
@@ -0,0 +1,16 @@
+/// <reference types="react" />
+
+declare global {
+  namespace JSX {
+    interface IntrinsicElements extends React.JSX.IntrinsicElements {}
+    interface Element extends React.JSX.Element {}
+    interface ElementClass extends React.JSX.ElementClass {}
+    interface ElementAttributesProperty extends React.JSX.ElementAttributesProperty {}
+    interface ElementChildrenAttribute extends React.JSX.ElementChildrenAttribute {}
+    interface LibraryManagedAttributes<C, P> extends React.JSX.LibraryManagedAttributes<C, P> {}
+    interface IntrinsicAttributes extends React.JSX.IntrinsicAttributes {}
+    interface IntrinsicClassAttributes<T> extends React.JSX.IntrinsicClassAttributes<T> {}
+  }
+}
+
+export {}; 
